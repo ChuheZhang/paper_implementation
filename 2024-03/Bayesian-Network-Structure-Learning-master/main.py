09919f3256c2d1a6608caefc7c5e0b5083b340f0
zd2002 <32207200078@e.gzhu.edu.cn>
2024-03-18 17:11:00 +0800

    fix

--- 2024-03/Bayesian-Network-Structure-Learning-master/main.py
+++ 2024-03/Bayesian-Network-Structure-Learning-master/main.py
@@ -1,21 +1,19 @@
 import numpy as np
 import pandas as pd
 import itertools
-from pomegranate import *
-from pomegranate import BayesianNetwork
+from pomegranate import *  # 导入pomegranate库，用于建模贝叶斯网络
 from collections import OrderedDict
 
-
-#data construction
-data = np.array(pd.read_csv("asia_data.csv"))
-card = data.max(0)
-data_capacity = []#all possible states for all nodes
+# 从CSV文件中加载数据并进行预处理
+data = np.array(pd.read_csv("asia_data.csv"))  # 读取数据
+card = data.max(0)  # 计算每列的最大值，代表每个变量的可能状态数
+data_capacity = []  # 初始化，用于存储所有节点的所有可能状态
 for i in range(len(card)):
-    data_capacity.append(list(np.arange(card[i])))
+    data_capacity.append(list(np.arange(card[i])))  # 为每个变量生成可能的状态列表
 data_capacity = np.array(data_capacity)    
-data = data-1
-#
+data = data-1  # 数据索引从0开始，因此对所有值减1以适应模型
 
+# 定义函数来计算条件参数（θ）
 def theta_copm(Pa,i,j,k):
     Pa_copy = Pa[i].copy()
     Pa_copy.append(i)
@@ -34,30 +32,30 @@ def theta_copm(Pa,i,j,k):
         Theta = 0
     return Theta
 
-
+# 计算累积分布函数（CDF）
 def cdf_comp(Pa,i,j,k):
     C = 0
     for k1 in range(k+1):
         C += theta_copm(Pa,i,j,k1)
     return C
 
-
-def influence_score(Pa,i,n,data_capacity,card):#n is the nth parent of ith node (pa[i][n])
+# 计算一个变量对其它变量的影响分数
+def influence_score(Pa,i,n,data_capacity,card):
     parents = Pa[i].copy()
     parents.pop(n)
-    parents_comb = list(itertools.product(*data_capacity[parents,:]))#all combinations of parents other than nth
+    parents_comb = list(itertools.product(*data_capacity[parents,:]))
     score = np.zeros(len(parents_comb))
     vote = np.zeros(len(parents_comb))
-    for p in range(len(parents_comb)):#set of fixed parent
+    for p in range(len(parents_comb)):
         c = np.zeros((card[Pa[i][n]],card[i]-1))
-        for s in range(card[Pa[i][n]]):#s is the state of the parent which we want to compute the influence score for
+        for s in range(card[Pa[i][n]]):
             for k in range(card[i]-1):
                 j = list(parents_comb[p]) + [s]
                 c[s,k] = cdf_comp(Pa,i,j,k)
-        if sum(sum((c[1:c.shape[0],:]-c[0:c.shape[0]-1,:])>=0))==(c.shape[0]-1)*c.shape[1]:#Ascending-->negative
+        if sum(sum((c[1:c.shape[0],:]-c[0:c.shape[0]-1,:])>=0))==(c.shape[0]-1)*c.shape[1]:
             score[p] = sum(c[0,:]-c[c.shape[0]-1,:])
             vote[p] = -1
-        if sum(sum((c[1:c.shape[0],:]-c[0:c.shape[0]-1,:])<=0))==(c.shape[0]-1)*c.shape[1]:#Descending-->positive
+        if sum(sum((c[1:c.shape[0],:]-c[0:c.shape[0]-1,:])<=0))==(c.shape[0]-1)*c.shape[1]:
             score[p] = sum(c[0,:]-c[c.shape[0]-1,:])
             vote[p] = 1
     Score = 0
@@ -65,24 +63,15 @@ def influence_score(Pa,i,n,data_capacity,card):#n is the nth parent of ith node 
         Score = sum(score)
     return Score
 
-
-
+# 计算模型中独立参数的数量
 def indep_params(Pa,card):    
-    params = 1#number of independent parameters
+    params = 1
     for i in range(len(Pa)):
         params*= (card[i]-1)*np.product(card[Pa[i]])
     return params
 
-
+# 检查图中是否存在循环
 def cyclic(g):
-    """Return True if the directed graph g has a cycle.
-    g must be represented as a dictionary mapping vertices to
-    iterables of neighbouring vertices. For example:
-    >>> cyclic({1: (2,), 2: (3,), 3: (1,)})
-    True
-    >>> cyclic({1: (2,), 2: (3,), 3: (4,)})
-    False
-    """
     path = set()
     def visit(vertex):
         path.add(vertex)
@@ -93,9 +82,8 @@ def cyclic(g):
         return False
     return any(visit(v) for v in g)
 
-
+# 计算贝叶斯信息准则（BIC）得分
 def BIC_score(data,Pa,card,struct):    
-    #
     z = []
     for ii in range(len(Pa)):
         zz = Pa[ii].copy()
@@ -111,16 +99,14 @@ def BIC_score(data,Pa,card,struct):
     for ii in range(len(z)):
         if len(z[ii])==len(z):
            ss=0
-    #    
-    model = BayesianNetwork()
     model = BayesianNetwork.from_structure(data,struct)
     BIC = model.log_probability(data).sum() - np.log(data.shape[0])*indep_params(Pa,card)/2 - ss
     return BIC
 
-
-def init_pop(ns,data,miu,sigma):#miu and sigma: mean and var of edge numbers in the network
-    n = data.shape[1]#number of nodes    
-    e = np.round(np.random.normal(miu,sigma,ns))#e is a vector with the same length as population which tells us how many edges are in every individual of population
+# 初始化种群
+def init_pop(ns,data,miu,sigma):
+    n = data.shape[1]   
+    e = np.round(np.random.normal(miu,sigma,ns))
     e[e>n*(n-1)/2] = n*(n-1)/2
     e[e<n-1] = n-1
     e = e.astype(np.int64)    
@@ -132,7 +118,7 @@ def init_pop(ns,data,miu,sigma):#miu and sigma: mean and var of edge numbers in 
         Pa.append([])
         for j in range(data.shape[1]):
             Pa[i].append([])    
-    for i in range(len(e)):#constructing population
+    for i in range(len(e)):
         s = np.random.randint(0,len(edges),2*len(edges))
         s = list(OrderedDict.fromkeys(s))
         s = s[:e[i]]    
@@ -142,10 +128,10 @@ def init_pop(ns,data,miu,sigma):#miu and sigma: mean and var of edge numbers in 
             Pa[i][all_edges[s[j]][choose_conj[j]]].append(all_edges[s[j]][conj+0])
     return Pa
 
-
-def mutation(individual):#add/remove one edge to/from the network
-    n = np.random.randint(0,len(individual))#the node which is going to mutate
-    binary = np.random.randint(0,1)#0: remove one edge #1: add one edge
+# 变异操作
+def mutation(individual):
+    n = np.random.randint(0,len(individual))
+    binary = np.random.randint(0,1)
     if (binary==0) and (len(individual[n])>0):
         individual[n] = list(np.delete(individual[n],-1))
     if (binary==1) and (len(individual[n])<len(individual)-1):
@@ -155,13 +141,13 @@ def mutation(individual):#add/remove one edge to/from the network
             individual[n] = list(set(individual[n]))   
     return individual
 
+# 交叉操作
 def crossover(individual1,individual2):
     n = np.random.randint(0,len(individual1))
     ind1 = individual1.copy()
     ind2 = individual2.copy()
     ind1[:n] = individual2[:n]
     ind2[:n] = individual1[:n]
-    #checking if there is an edge toward the node itself, then undo crossover
     s1 = 0
     s2 = 0
     for i in range(len(individual1)):
@@ -174,7 +160,8 @@ def crossover(individual1,individual2):
     if s2>0:
         ind2 = individual2        
     return ind1,ind2
-   
+
+# 选择操作
 def selection(pop,fitness,ns):
     fitness1 = -fitness
     fitness1 = fitness1.max()-fitness1
@@ -190,9 +177,7 @@ def selection(pop,fitness,ns):
     selected_pop = [pop[i] for i in selected_ind]    
     return selected_pop,selected_ind     
 
-
-
-#evaluation of population
+# 评估种群
 def pop_eval(Pa,ns,data,card):
     BICs = []
     ind = []
@@ -201,27 +186,23 @@ def pop_eval(Pa,ns,data,card):
         g = {}
         for j in range(len(struct)):
             g[j] = struct[j]
-        if cyclic(g) is False:    
+        if not cyclic(g):    
             BICs.append(BIC_score(data,Pa[i],card,struct))
             ind.append(i)
     BICs = np.array(BICs)
     pop = [Pa[i] for i in ind]
     return BICs,pop
 
-
-
-
-
-#initialization
-pr = 0.2#mutation probability
-miu = 10
-sigma = 3
-ns = 400#population size
-Pa = init_pop(ns,data,miu,sigma)
-#evaluation of initial population
-BICs,pop = pop_eval(Pa,ns,data,card)
+# 主程序开始
+# 初始化参数
+pr = 0.2  # 变异概率
+miu = 10  # 边缘数的平均值
+sigma = 3  # 边缘数的标准差
+ns = 400  # 种群大小
+Pa = init_pop(ns,data,miu,sigma)  # 初始化种群
+BICs, pop = pop_eval(Pa, ns, data, card)  # 评估初始种群
 ns = len(pop)
-if ns%2!=0:
+if ns % 2 != 0:
     ns -= 1
 best_ind = BICs.argmax()
 best_individual = pop[best_ind]
@@ -229,18 +210,14 @@ best_val = BICs.max()
 bests = [best_val]
 for iteration in range(10):
     print('iteration=', iteration)
-    #selection
-    selected_pop,selected_ind = selection(pop,BICs,ns)
-    #mutation
+    selected_pop, selected_ind = selection(pop, BICs, ns)  # 选择
     for i in range(ns):
-        if np.random.uniform(0, 1)<pr:
-            selected_pop[i] = mutation(selected_pop[i])
-    #crossover
+        if np.random.uniform(0, 1) < pr:
+            selected_pop[i] = mutation(selected_pop[i])  # 变异
     selected_pop1 = selected_pop.copy()
-    for i in range(int(ns/2)):
-        selected_pop1[i*2],selected_pop1[i*2+1] = crossover(selected_pop1[i*2],selected_pop1[i*2+1])     
-    #evaluation and elitism
-    BICs,pop = pop_eval(selected_pop1,ns,data,card)
+    for i in range(int(ns / 2)):
+        selected_pop1[i * 2], selected_pop1[i * 2 + 1] = crossover(selected_pop1[i * 2], selected_pop1[i * 2 + 1])  # 交叉
+    BICs, pop = pop_eval(selected_pop1, ns, data, card)  # 评估种群并实现精英主义
     ind = BICs.argmin()
     pop[ind] = best_individual
     BICs[ind] = best_val
@@ -249,35 +226,16 @@ for iteration in range(10):
     best_val = BICs.max()
     bests.append(best_val)
 
-
-
 a = np.argsort(BICs)
 aa = BICs[a]
 bb = [pop[i] for i in a]
-#print(BICs[a[95]])
-#print(pop[a[95]])
-##
 p = [[],[0],[],[2],[2],[1,3],[5],[5,4]]
-#p = bb[-1]
 struct = tuple(tuple(x) for x in p) 
+print(BIC_score(data, p, card, struct))
 
-print(BIC_score(data,p,card,struct))
-
-
-
-
-Score = 
+# 计算影响分数
+Score = 0
 for i in range(8):
-    if len(bb[156][i])>0:
+    if len(bb[156][i]) > 0:
         for n in range(len(bb[156][i])):
-            print(influence_score(bb[156],i,1,data_capacity,card))
-#Score = 0
-#for i in range(data.shape[1]):
-#    for n in range(len(Pa[i])):
-##        Score += influence_score(Pa,i,n,data_capacity,card)
-#        print(influence_score(Pa,i,n,data_capacity,card))
-        
-        
-#
-#Theta = theta_copm(Pa,i,j,k)
-#c = cdf_comp(Pa,i,j,k)
+            print(influence_score(bb[156], i, 1, data_capacity, card))
